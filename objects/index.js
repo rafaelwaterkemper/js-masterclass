d03e6bf487e27b6e15ed60adbd774df93f2af169
rafaelwaterkemper <rafaelwaterkemper@gmail.com>
2021-09-12 03:30:04 -0300

    Add algorithm to compare two objects

--- objects/index.js
+++ objects/index.js
@@ -8,11 +8,11 @@ console.log({
 })
 
 const book = {}
-let propAuthor = 'author'
+let propAuthor = "author"
 
-book.title = 'Test'
-book['pages'] = 65
-book[propAuthor] = 'Robert C. Martin'
+book.title = "Test"
+book["pages"] = 65
+book[propAuthor] = "Robert C. Martin"
 
 console.log(book)
 
@@ -28,3 +28,34 @@ for (let key in book1) {
     book2[key] = book1[key];
 }
 console.log(book2);
+
+console.log("title" in book2)
+console.log("publisher" in book2)
+
+const bookTest1 = {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+};
+
+const bookTest2 = {
+    title: "Clean Codee",
+    author: "Robert C. Martin",
+};
+
+console.log(bookTest1 == bookTest2) //false
+console.log(bookTest1 === bookTest2) //false
+
+function equals(obj, comparable) {
+
+    for (let key in obj) {
+        if (obj[key] !== comparable[key]) return false
+    }
+
+    for (let key in comparable) {
+        if (obj[key] !== comparable[key]) return false
+    }
+
+    return true
+}
+
+console.log(equals(bookTest1, bookTest2))
