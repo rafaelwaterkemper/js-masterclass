0304acfa8c7298606e4aac4e065934c8106d9661
rafaelwaterkemper <rafaelwaterkemper@gmail.com>
2021-09-15 11:10:55 -0300

    Add some anotattions about prototype

--- objects/prototype.js
+++ objects/prototype.js
@@ -1,3 +1,7 @@
+//Prototype from constructor function take the prototype from Constructor Object;
+//Updating methods from prototype through obj or Constructor Object will reflect all objects 
+//that have the prototype in your prototype chain
+
 Object.create({}) //Care with null, because its remove default methods from object
 
 let functionLanguage = {
@@ -37,3 +41,30 @@ console.log(scheme)
 console.log(scheme.paradigm)
 console.log(perl)
 console.log(perl.paradigm)
+
+let Person = function(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+let p1 = new Person("Rafa", 12);
+p1.__proto__.getName = function () {
+    return this.name;
+}
+console.log(p1.__proto__ === Person.prototype)
+console.log(p1.getName())
+
+let p2 = new Person("Guga", 13);
+
+console.log(p2.__proto__ === Person.prototype)
+console.log(p2.getName())
+
+let p1Factory = {name: "Salete", age: 51}
+console.log(p1Factory.__proto__ === Object.prototype)
+p1Factory.__proto__.otherMethodObject = function () {
+    return "inside object"
+}
+console.log(p1Factory.otherMethodObject())
+let p2Factory = {name: "Dirceu", age: 51}
+console.log(p2Factory.otherMethodObject())
+console.log(p2.otherMethodObject())
